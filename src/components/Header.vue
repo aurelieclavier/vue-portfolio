ae07fb29d10d16ac112de334d21aaadcdcf02ca8
aurelie <aurelie.cissokho@gmail.com>
2019-05-16 14:04:40 +0200

    integration header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,12 @@
 <template>
-    <header id="header" class="alt">
-        <h1>{{ name }}
-          <span class="hidden-mobil">
-             -
-          </span>
-          <br class="hidden-tablet">
-          {{ job }}
-        </h1>
-        <p>{{ presentation }}</p>
-    </header>
+  <header id="header" class="alt">
+    <svg>
+      <text text-anchor="middle" x="50%" y="90%" class="name">{{ name }}</text>
+    </svg>
+    <svg>
+      <text text-anchor="middle" x="50%" y="50%" class="job">{{ job }}</text>
+    </svg>
+  </header>
 </template>
 
 
@@ -18,8 +16,7 @@ export default {
   data () {
     return {
       name: 'Aurélie',
-      job: 'Développeur web',
-      presentation: 'Bonjour, j\'ai 29 ans et je vis à Marseille.'
+      job: 'Développeuse front-end'
     }
   }
 }
