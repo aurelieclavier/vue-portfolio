a6126ff3290d9c19720788af341bf962b303653c
aurelie <aurelie.cissokho@gmail.com>
2019-05-17 11:56:56 +0200

    modification component and style of skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,36 +1,17 @@
 <template>
     <section id="skills" class="main">
         <header class="major">
-            <h2>Mes compétences</h2>
+            <h2>Languages</h2>
         </header>
         <ul class="features">
-            <li>
+            <li v-for="item in items" :key="item.id">
                 <div>
-                    <font-awesome-icon prefix="fa" icon="code" class="icon icon-code"/>
+                    <font-awesome-icon prefix="fa" v-bind:icon="item.icon" v-bind:class="item.classIcon"/>
                 </div>
-                <h3>Langages</h3>
-                <p>HTML 5, CSS 3, PHP 7, JavaScript, SQL.</p>
-            </li>
-            <li>
-                <div>
-                    <font-awesome-icon prefix="fa" icon="cogs" class="icon icon-cogs"/>
+                <h3>{{ item.h3 }}</h3>
+                <div class="featuresDetail">
+                    <p v-for="txt in item.text" :key="txt.id" >{{ txt.label }}</p>
                 </div>
-                <h3>Frameworks / CMS</h3>
-                <p>VueJs, Ionic, Symfony 3, Wordpress.</p>
-            </li>
-            <li>
-                <div>
-                    <font-awesome-icon prefix="fa" icon="plus" class="icon icon-plus"/>
-                </div>
-                <h3>Librairies</h3>
-                <p>Jquery, Leaflet, Less, Sass, BootStrap, Buefy, Vuetify, Materialize, Slick.</p>
-            </li>
-            <li>
-                <div>
-                    <font-awesome-icon prefix="fa" icon="wrench" class="icon icon-wrench"/>
-                </div>
-                <h3>Outils</h3>
-                <p>MySQL Workbench, Git, GitLab, Redmine, Gescom, Photoshop.</p>
             </li>
         </ul>
     </section>
@@ -38,7 +19,139 @@
 
 <script>
 export default {
-    name: 'SkillsComponent'
+    name: 'SkillsComponent',
+    data () {
+        return {
+            items: [
+                {
+                    id: 0,
+                    icon: 'code',
+                    classIcon: 'icon icon-code',
+                    h3: 'Languages',
+                    text: [
+                        {
+                            id: 0,
+                            label: 'HTML 5'
+                        },
+                        {
+                            id: 1,
+                            label: 'CSS 3'
+                        },
+                        {
+                            id: 2,
+                            label: 'JavaScript'
+                        },
+                        {
+                            id: 3,
+                            label: 'PHP 7'
+                        },
+                        {
+                            id: 4,
+                            label: 'SQL'
+                        },
+                    ],
+                },
+                {
+                    id: 2,
+                    icon: 'cogs',
+                    classIcon: 'icon icon-cogs',
+                    h3: 'Frameworks / CMS',
+                    text: [
+                        {
+                            id: 0,
+                            label: 'VueJs'
+                        },
+                        {
+                            id: 1,
+                            label: 'Angular 2+'
+                        },
+                        {
+                            id: 2,
+                            label: 'Ionic'
+                        },
+                        {
+                            id: 3,
+                            label: 'Symfony 3'
+                        },
+                        {
+                            id: 4,
+                            label: 'Wordpress'
+                        },
+
+                    ],
+                },
+                {
+                    id: 3,
+                    icon: 'plus',
+                    classIcon: 'icon icon-plus',
+                    h3: 'Librairies',
+                    text: [
+                        {
+                            id: 0,
+                            label: 'Materialize'
+                        },
+                        {
+                            id: 1,
+                            label: 'Material Design'
+                        },
+                        {
+                            id: 2,
+                            label: 'Sass'
+                        },
+                        {
+                            id: 3,
+                            label: 'Less'
+                        },
+                        {
+                            id: 4,
+                            label: 'BootStrap'
+                        },
+                        {
+                            id: 5,
+                            label: 'JQuery'
+                        }
+
+                    ],
+                },
+                {
+                    id: 4,
+                    icon: 'wrench',
+                    classIcon: 'icon icon-wrench',
+                    h3: 'Outils',
+                    text: [
+                        {
+                            id: 0,
+                            label: 'MySQL Workbench'
+                        },
+                        {
+                            id: 1,
+                            label: 'Git'
+                        },
+                        {
+                            id: 2,
+                            label: 'GitLab'
+                        },
+                        {
+                            id: 3,
+                            label: 'Mercurial'
+                        },
+                        {
+                            id: 4,
+                            label: 'Gescom'
+                        },
+                        {
+                            id: 5,
+                            label: 'Redmine'
+                        },
+                        {
+                            id: 6,
+                            label: 'Photoshop'
+                        },
+                    ],
+                }
+            ]
+        }
+    }
 }
 </script>
 
