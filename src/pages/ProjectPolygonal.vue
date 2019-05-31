08f894f33b565fe22525ad7f69edfed2ab61b510
aurelie <aurelie.cissokho@gmail.com>
2019-05-31 16:26:25 +0200

    add assets in ProjectPolygonalComponent

--- src/pages/ProjectPolygonal.vue
+++ src/pages/ProjectPolygonal.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="wrapper">
-    <header id="header">
-      <h1>Polygonal-Design</h1>
+    <header id="header" class="alt">
+      <svg>
+        <text text-anchor="middle" x="50%" y="50%" class="title">Polygonal-Design</text>
+      </svg>
     </header>
     <main id="main">
       <ProjectPolygonalComponent></ProjectPolygonalComponent>
