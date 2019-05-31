d776310e51c34ba3d62ff1ca7e241877a8b03817
aurelie <aurelie.cissokho@gmail.com>
2019-05-31 16:30:29 +0200

    add button back home page on ProjectPolygonalComponent

--- src/components/ProjectPolygonal.vue
+++ src/components/ProjectPolygonal.vue
@@ -1,13 +1,30 @@
 <template>
-    <section>
-        <img src="../assets/img/job/polygonal/polygonal.jpg" alt="">
-        <img src="../assets/img/job/polygonal/unfold1.jpg" alt="">
-        <img src="../assets/img/job/polygonal/unfold2.jpg" alt="">
+    <section id="content" class="main">
+        <div>
+            <img src="../assets/img/job/polygonal/polygonal.jpg" alt="">
+            <img src="../assets/img/job/polygonal/unfold1.jpg" alt="">
+            <img src="../assets/img/job/polygonal/unfold2.jpg" alt="">
+        </div>
+        <!-- backButton -->
+        <div class="backButton">
+                <router-link :to="lien.url" class="router">
+                {{ lien.label }}
+                </router-link>
+        </div>
     </section>
 </template>
 
 <script>
 export default {
-    name: 'ProjectPolygonalComponent'
+    name: 'ProjectPolygonalComponent',
+    data() {
+        return {
+            lien:{
+                id: 0,
+                label: 'Retour',
+                url: '/'
+            }
+        }
+    }
 }
 </script>
