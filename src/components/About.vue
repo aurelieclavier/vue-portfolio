06b08fc05596070e50284f0b66d34221cf44ff94
aurelieclavier <aurelie.cissokho@gmail.com>
2020-02-09 23:22:10 +0100

    modification component and add method counter date

--- src/components/About.vue
+++ src/components/About.vue
@@ -33,8 +33,46 @@ export default {
             },
             content:{
                 title: 'Mon histoire',
-                text: 'Il y a un peu plus d\'un an maintenant que j\'ai choisi de devenir développeuse web. Pourquoi? Pour son aspect créatif, pour sa compléxité, pour comprendre comment tout ça fonctionnait et aussi pour être dans un univers qui me correspondait.'
+                text: 'Il y a maintenant ' + this.counter() + 'que j\'ai choisi de devenir développeuse web. Pourquoi? Pour son aspect créatif, pour sa compléxité, pour comprendre comment tout ça fonctionnait et aussi pour être dans un univers qui me correspondait.'
             }
+        }
+    },
+    methods: {
+        counter() {
+            var theDate = new Date('10-02-2017');
+            var _second = 1000;
+            var _minute = _second * 60;
+            var _hour = _minute * 60;
+            var _day = _hour * 24;
+            var _year = _day * 365;
+            var timer;
+
+            function count() {
+                var now = new Date();
+                if (theDate > now) {
+                var distance = theDate - now;
+                    if (distance < 0) {
+                        clearInterval(timer);
+                        return;
+                    }
+                } else {
+                    var distance = now - theDate;
+                    if (distance < 0) {
+                        clearInterval(timer);
+                        return;
+                    }
+                }
+                var years = Math.floor(distance / _year);
+                var days=Math.floor((distance/(60*60*1000*24)*1)-(365*years));
+                var daytext = '';
+                if (days > 1) {
+                    daytext = ' jours ';
+                } else {
+                    daytext = ' jour ';
+                }                
+                return years + ' années et '  + days + daytext;
+            }
+            return count();
         }
     }
 }
