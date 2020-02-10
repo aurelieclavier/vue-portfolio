10dd508bd00e4d6cbc4cbf9e03bb9c0929b7a07b
aurelieclavier <aurelie.cissokho@gmail.com>
2020-02-10 22:17:41 +0100

    ddefine variable for footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,31 +3,16 @@
         <section id="cv">
             <article class="major">
                 <h2>Mon CV</h2>
-                <a href="files/cv.pdf" target="_blank">
-                    <img src="../assets/img/cv.png" alt="cv aurelie" class="img-border-radius">
+                <a :href="cv.link" target="_blank">
+                    <img :src="cv.image" :alt="cv.alt" class="img-border-radius">
                 </a>
             </article>
         </section>
         <section>
             <ul class="icons">
-                <li>
-                    <a href="tel:+33651014007" target="_blank">
-                        <img src="../assets/img/reseaux/call.png" alt="">
-                    </a>
-                </li>
-                <li>
-                    <a href="mailTo:c.aurelie.pro@gmail.com" target="_blank">
-                        <img src="../assets/img/reseaux/mail.png" alt="">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/aur%C3%A9lie-clavier-64bab8151/" target="_blank">
-                        <img src="../assets/img/reseaux/linkedin.png" alt="">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/aurelieclavier" target="_blank">
-                        <img src="../assets/img/reseaux/github.png" alt="">
+                <li v-for="item in items" :key="item.id">
+                    <a v-bind:href="item.link" target="_blank">
+                        <img :src="item.image" :alt="item.alt">
                     </a>
                 </li>
             </ul>
@@ -37,7 +22,43 @@
 
 <script>
 export default {
-    name: 'FooterComponent'
+    name: 'FooterComponent',
+    data () {
+        return {
+            items: [
+                {
+                    id: 0,
+                    link: "tel:+33651014007",
+                    image: [require("../assets/img/reseaux/call.png")],
+                    alt: "Téléphone Aurélie"
+                },
+                {
+                    id: 1,
+                    link: "mailTo:c.aurelie.pro@gmail.com",
+                    image: [require("../assets/img/reseaux/mail.png")],
+                    alt: "Email Aurélie"
+                },
+                {
+                    id: 2,
+                    link: "https://www.linkedin.com/in/aurelieclavier/",
+                    image: [require("../assets/img/reseaux/linkedin.png")],
+                    alt: "Linkedin Aurélie"
+                },
+                {
+                    id: 3,
+                    link: "https://github.com/aurelieclavier",
+                    image: [require("../assets/img/reseaux/github.png")],
+                    alt: "Github Aurélie"
+                }
+            ],
+            cv: {
+                id: 0,
+                link: "files/cv.pdf",
+                image: [require("../assets/img/cv.png")],
+                alt: "cv aurelie"     
+            }
+        }
+    }
 }
 </script>
 
