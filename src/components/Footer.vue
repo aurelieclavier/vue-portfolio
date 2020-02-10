8f51f8cf69e3985cc7e46f2641f8cf3263d61d74
aurelieclavier <aurelie.cissokho@gmail.com>
2020-02-10 21:54:22 +0100

    new tamplate for contact section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,35 +4,30 @@
             <article class="major">
                 <h2>Mon CV</h2>
                 <a href="files/cv.pdf" target="_blank">
-                    <font-awesome-icon prefix="fa" icon="file-pdf" class="icon icon-file-text"/>
+                    <img src="../assets/img/cv.png" alt="cv aurelie" class="img-border-radius">
                 </a>
             </article>
         </section>
         <section>
-            <ul class="contact">
-                <li>
-                    <font-awesome-icon prefix="fa" icon="phone" class="icon icon-phone"/>
-                    <a href="tel:+33651014007">Apellez-moi</a>
-                </li>
-                <li>
-                    <font-awesome-icon prefix="fa" icon="at" class="icon icon-at"/>
-                    <a href="mailTo:c.aurelie.pro@gmail.com">Maillez-moi</a>
-                </li>
-            </ul>
             <ul class="icons">
                 <li>
-                    <a href="https://www.linkedin.com/in/aur%C3%A9lie-clavier-64bab8151/" class="icon fa-linkedin alt" target="_blank">
-                        <font-awesome-icon prefix="fa" icon="linkedin" class="icon icon-linkedin"/>
+                    <a href="tel:+33651014007" target="_blank">
+                        <img src="../assets/img/reseaux/call.png" alt="">
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/aurelieclavier" class="icon fa-github alt" target="_blank">
-                        <font-awesome-icon prefix="fa" icon="github" class="icon icon-github"/>
+                    <a href="mailTo:c.aurelie.pro@gmail.com" target="_blank">
+                        <img src="../assets/img/reseaux/mail.png" alt="">
                     </a>
                 </li>
                 <li>
-                    <a href="https://codepen.io/aurelieclavier/" class="icon fa-codepen alt" target="_blank">
-                        <font-awesome-icon prefix="fa" icon="codepen" class="icon icon-codepen"/>
+                    <a href="https://www.linkedin.com/in/aur%C3%A9lie-clavier-64bab8151/" target="_blank">
+                        <img src="../assets/img/reseaux/linkedin.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/aurelieclavier" target="_blank">
+                        <img src="../assets/img/reseaux/github.png" alt="">
                     </a>
                 </li>
             </ul>
