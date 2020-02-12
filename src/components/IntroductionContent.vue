5414212a0d3c9b87d1a622d316b015b4808ddbf7
aurelieclavier <aurelie.cissokho@gmail.com>
2020-02-12 19:49:31 +0100

    add tags for SEO referencement

--- src/components/IntroductionContent.vue
+++ src/components/IntroductionContent.vue
@@ -7,7 +7,7 @@
         <div v-for="item in items" :key="item.id">
             <h2>{{ item.title }}</h2>
             <div v-for="txt in item.content" v-bind:key="txt.id">
-                <p>{{ txt.text }}</p>
+                <p v-html="txt.text"></p>
             </div>
         </div>
         <div class="backButton">
@@ -40,11 +40,11 @@ export default {
                         },
                         {
                             id: 1,
-                            text: "Il y a quelques temps maintenant que j'ai choisi de devenir développeuse. Pourquoi ? Pour son aspect créatif, pour sa complexité, pour comprendre comment fonctionne les applications et aussi pour être dans un univers qui me correspond."
+                            text: "Il y a quelques temps maintenant que j'ai choisi de devenir <strong class='text-color'>développeuse</strong>. Pourquoi ? Pour son aspect créatif, pour sa complexité, pour comprendre comment fonctionnent les applications et aussi pour être dans un univers qui me correspond."
                         },
                         {
                             id: 2,
-                            text: "J'ai donc jeté mon dévolu sur la 3W Academy pour suivre ma formation. Celle-ci a duré trois mois, d'octobre à décembre 2017, durant lesquels nous avons appris les bases des principaux langages web (HTML, CSS, JavaScript, PHP et SQL). A la fin de ces 3 mois intensifs, j'ai obtenu un titre RNCP niveau bac+2 développeur intégrateur en réalisation d'application web."
+                            text: "J'ai donc jeté mon dévolu sur la <strong class='text-color'><a href='https://3wa.fr/' target='_blank' class='link'>3W Academy</a></strong> pour suivre ma formation. Celle-ci a duré trois mois, d'octobre à décembre 2017, durant lesquels nous avons appris les bases des principaux langages web <strong class='text-color'>(HTML, CSS, JavaScript, PHP et SQL)</strong>. A la fin de ces 3 mois intensifs, j'ai obtenu un titre RNCP niveau bac+2 développeur intégrateur en réalisation d'application web."
                         }
                     ]
                 },{
@@ -53,7 +53,7 @@ export default {
                     content: [
                         {
                             id: 0,
-                            text: "En janvier 2018, j'ai poursuivi ma route avec un stage de trois mois chez Polygonal-Design. Pendant cette période, j'ai effectué une refonte complète de leur site web en totale autonomie. Le site est maintenant une vraie boutique en ligne avec un joli panier dynamique développé en Vue.JS."
+                            text: "En janvier 2018, j'ai poursuivi ma route avec un stage de trois mois chez <strong class='text-color'><a href='https://www.polygonal-design.eu/' target='_blank' class='link'>Polygonal-Design</strong></a>. Pendant cette période, j'ai effectué une refonte complète de leur site web en totale autonomie. Le site est maintenant une vraie boutique en ligne avec un joli panier dynamique développé en Vue.JS."
                         },
                         {
                             id: 1,
@@ -61,7 +61,7 @@ export default {
                         },
                         {
                             id: 2,
-                            text: "À la suite de cette première expérience, j'ai pris du temps pour concrétiser différents projets personnels et continuer à me former. J'ai obtenu une certification Angular après avoir suivi une formation en ligne sur le site d'OpenClassRooms et je me suis auto-formée sur Vue.JS"
+                            text: "À la suite de cette première expérience, j'ai pris du temps pour concrétiser différents projets personnels et continuer à me former. J'ai suivi et complété une formation <strong class='text-color'>Angular</strong> sur le site d'OpenClassRooms et je me suis auto-formée sur <strong class='text-color'>Vue.JS</strong>"
                         },
                         {
                             id: 3,
@@ -74,11 +74,11 @@ export default {
                     content: [
                         {
                             id: 0,
-                            text: "Après mon passage chez Jaguar-Network, j'ai choisi de sortir de ma zone de confort et de m'orienter vers les langages back-end. Après quelques recherches, j'ai pu voir que le langage Java était très utilisé. J'ai donc choisi de me former sur cette technologie."
+                            text: "Après mon passage chez Jaguar-Network, j'ai choisi de sortir de ma zone de confort et de m'orienter vers les langages <strong class='text-color'>back-end</strong>. Après quelques recherches, j'ai pu voir que le <strong class='text-color'>langage Java</strong> était très utilisé. J'ai donc choisi de me former sur cette technologie."
                         },
                         {
                             id: 1,
-                            text: "J'ai eu le plaisir de suivre une formation de développeur Java à la Wild Code School. Pendant ces trois mois nous avons pu apprendre à coder en Java et se familiariser entre autres avec le framework Spring ou encore Hibernate."
+                            text: "J'ai eu le plaisir de suivre une formation de développeur Java à la <strong class='text-color'><a href='https://www.wildcodeschool.com/fr-FR' target='_blank' class='link'>Wild Code School</a></strong>. Pendant ces trois mois nous avons pu apprendre à coder en Java et se familiariser entre autres avec le framework <strong class='text-color'>Spring</strong> ou encore <strong class='text-color'>Hibernate</strong>."
                         },
                         {
                             id: 2,
@@ -86,7 +86,7 @@ export default {
                         },
                         {
                             id: 3,
-                            text: "Java a été pour moi un réel coup de cœur. N'hésitez pas me contacter pour plus de renseignements ou si mon profil vous intéresse."
+                            text: "<strong class='text-color'>Java a été pour moi un réel coup de cœur</strong>. N'hésitez pas me contacter pour plus de renseignements ou si mon profil vous intéresse."
                         },
                     ]
                 }
