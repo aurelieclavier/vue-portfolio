e5ec2f72e8180d00f82e7469202ed7db4ff6bca9
aurelieclavier <aurelie.cissokho@gmail.com>
2020-02-12 12:19:52 +0100

    define variable in template

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,40 +1,15 @@
 <template>
     <nav id="nav">
         <ul>
-            <li id="1"
-            :class="{ active : active_el == 1 }"
-            class="nav-link">
-                <a href="#intro"
-                @click="activate(1)"
-                v-smooth-scroll="{ duration: 1000, offset: -80}">Introduction</a>
-            </li>
-            <li id="2"
-            :class="{ active : active_el == 2 }"
-            class="nav-link">
-                <a href="#skills"
-                @click="activate(2)"
-                v-smooth-scroll="{ duration: 1000, offset: -80}">Compétences</a>
-            </li>
-            <li id="3"
-            :class="{ active : active_el == 3 }"
-            class="nav-link">
-                <a href="#exp"
-                @click="activate(3)"
-                v-smooth-scroll="{ duration: 1000, offset: -80}">Expériences</a>
-            </li>
-            <li id="4"
-            :class="{ active : active_el == 4 }"
-            class="nav-link">
-                <a href="#project"
-                @click="activate(4)"
-                v-smooth-scroll="{ duration: 1000, offset: -80}">Projets</a>
-            </li>
-            <li id="4"
-            :class="{ active : active_el == 5 }"
-            class="nav-link">
-                <a href="#contact"
-                @click="activate(5)"
-                v-smooth-scroll="{ duration: 1000}">Contact</a>
+            <li v-for="link in links" :key="link.id"
+                :id="link.id" 
+                :class="{ active : active_el == link.active_el }" 
+                class="nav-link">
+                <a v-bind:href="link.href" 
+                @click="link.click" 
+                v-smooth-scroll="{ duration: 1000, offset: -50}">
+                    {{ link.content }}
+                </a>
             </li>
         </ul>
     </nav>
@@ -45,7 +20,44 @@ export default {
     name: 'NavComponent',
     data () {
         return {
-            active_el: 1
+            active_el: 1,
+            links: [
+                {
+                    id : 1,
+                    active_el: 1,
+                    click : "activate(1)",
+                    href: "#intro",
+                    content: "Introduction"
+                },
+                {
+                    id: 2,
+                    active_el: 2,
+                    click : "activate(2)",
+                    href: "#skills",
+                    content: "Compétences"
+                },
+                {
+                    id: 3,
+                    active_el: 3,
+                    click : "activate(3)",
+                    href: "#exp",
+                    content: "Expériences"
+                },
+                {
+                    id: 4,
+                    active_el: 4,
+                    click : "activate(4)",
+                    href: "#project",
+                    content: "Projets"
+                },
+                {
+                    id: 5,
+                    active_el: 5,
+                    click : "activate(5)",
+                    href: "#contact",
+                    content: "Contact"
+                },
+            ]
         }
     },
     methods: {
